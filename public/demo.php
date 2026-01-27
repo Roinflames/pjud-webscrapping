a902c9e4612af442644179ae33fef513d0eba5e2
Diettoh <67396128+Diettoh@users.noreply.github.com>
2026-01-27 19:35:39 -0300

    Snapshot V49

--- public/demo.php
+++ public/demo.php
@@ -8,7 +8,7 @@
  */
 
 // Cargar variables de entorno desde .env
-$envFile = __DIR__ . '/../../.env';
+$envFile = __DIR__ . '/../.env';
 if (file_exists($envFile)) {
     $lines = file($envFile, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
     foreach ($lines as $line) {
@@ -19,7 +19,7 @@ if (file_exists($envFile)) {
 }
 
 // Cargar autoloader de las clases del proyecto
-require_once __DIR__ . '/../src/autoload.php';
+require_once __DIR__ . '/src/autoload.php';
 
 use App\Repository\CausaRepository;
 use App\Repository\MovimientoRepository;
